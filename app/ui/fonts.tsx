3205d60a7fbcdbc3e34d735db51fe0dc629e95a6
raozhaizhu <raozhaizhu@gmail.com>
2025-02-15 15:43:16 +0800

    init

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,6 @@
+import { Inter, Lusitana, Corinthia } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const corinthia = Corinthia({ subsets: ['latin'], weight: ['400', '700'] });
+
